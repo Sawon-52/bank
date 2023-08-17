e86ad4589998c456197288045362bf718ec27d6e
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-08-17 13:17:48 +0600

    balence manement with js

--- script.js
+++ script.js
@@ -13,7 +13,36 @@ document.getElementById('deposite-btn').addEventListener('click', function(){
     const currentDepositeTotal = previousDepositeAmmount + newDepositeAmmount;
     depositeField.innerText = currentDepositeTotal;
 
+    // add balance 
+    
+    const previousBalanceElement = document.getElementById('balance-total');
+    const previousBalanceString = previousBalanceElement.innerText;
+    const previousBalance = parseFloat(previousBalanceString);
+
+    const totalBalance = currentDepositeTotal + previousBalance ;
+    previousBalanceElement.innerText = totalBalance;
+
+
     // clear the deposite field
     depositInputForm.value = '';
-
 })
+
+document.getElementById('withdrow-btn').addEventListener('click', function(){
+    //console.log('clicked');
+    const depositInputForm = document.getElementById('deposite-form');
+    const newDepositeAmmountString = depositInputForm.value;
+    const newDepositeAmmount = parseFloat(newDepositeAmmountString);
+    // console.log(depositeValue);
+
+    const depositeField = document.getElementById('deposite-amount');
+    const previousDepositeAmmountString = depositeField.innerText;
+    const previousDepositeAmmount = parseFloat(previousDepositeAmmountString);
+    // console.log(previousDepositeAmmount);
+
+    const currentDepositeTotal = previousDepositeAmmount + newDepositeAmmount;
+    depositeField.innerText = currentDepositeTotal;
+
+    // clear the deposite field
+    depositInputForm.value = '';
+})
+
